ddff5d50e3de966d7039963f621d68676e8f1776
Faith Kimani <wanjikukimani24@gmail.com>
2025-02-11 04:21:32 +0000

    nested object destructuring

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,14 @@ import About from "./About";
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
+const {name,bio, city,color, links} = user
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color ={color} name={name} city={city}/>
+      <About bio={bio} links = {links}/>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+function Links({github, linkedin}){
+    return(
+        
+        <div>
+            <h3>Links</h3>
+            <a href={github}>{github}</a>
+            <a href={linkedin}>{linkedin}</a>
+        </div>
+    )
+}
+
+export default Links
